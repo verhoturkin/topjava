26b71f81022ac39326e668da8d6bb30bd696e0d8
Verkhoturkin <verhoturkin@gmail.com>
2019-06-14 14:20:11 +0500

    HW02 done service

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,4 +1,22 @@
 package ru.javawebinar.topjava.service;
 
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.util.List;
+import java.util.function.Predicate;
+
 public interface MealService {
+
+    Meal create(Meal meal, int userId);
+
+    void delete(int id, int usedId) throws NotFoundException;
+
+    Meal get(int id, int userId) throws NotFoundException;
+
+    void update(Meal meal, int userId);
+
+    List<Meal> getAll(int userId);
+
+    List<Meal> getFiltered(int userId, Predicate<Meal> filter);
 }
--- src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
@@ -1,9 +1,53 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
+import java.util.List;
+import java.util.function.Predicate;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
+
+@Service
 public class MealServiceImpl implements MealService {
 
-    private MealRepository repository;
+    private final MealRepository repository;
 
+    @Autowired
+    public MealServiceImpl(MealRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Meal create(Meal meal, int userId) {
+        return repository.save(meal, userId);
+    }
+
+    @Override
+    public void delete(int id, int usedId) throws NotFoundException {
+        checkNotFoundWithId(repository.delete(id, usedId), id);
+    }
+
+    @Override
+    public Meal get(int id, int userId) throws NotFoundException {
+        return checkNotFoundWithId(repository.get(id, userId), id);
+    }
+
+    @Override
+    public void update(Meal meal, int userId) {
+        checkNotFoundWithId(repository.save(meal, userId), meal.getId());
+    }
+
+    @Override
+    public List<Meal> getAll(int userId) {
+        return repository.getAll(userId);
+    }
+
+    @Override
+    public List<Meal> getFiltered(int userId, Predicate<Meal> filter) {
+        return repository.getFiltered(userId, filter);
+    }
 }
