9b104b11be0eadb5bd174bc827b3f7695cdeaeb6
Verkhoturkin <verhoturkin@gmail.com>
2019-07-13 18:59:50 +0300

    split tests by ORM realization

---
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DatajpaMealServiceTest.java
@@ -0,0 +1,7 @@
+package ru.javawebinar.topjava.service.datajpa;
+
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.service.MealServiceTest;
+@ActiveProfiles("datajpa")
+public class DatajpaMealServiceTest extends MealServiceTest {
+}
---
+++ src/test/java/ru/javawebinar/topjava/service/jpa/JpaMealServiceTest.java
@@ -0,0 +1,7 @@
+package ru.javawebinar.topjava.service.jpa;
+
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.service.MealServiceTest;
+@ActiveProfiles("jpa")
+public class JpaMealServiceTest extends MealServiceTest {
+}
